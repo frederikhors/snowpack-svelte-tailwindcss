e0bfa569b83cd258e42adedf1b7be555d9eab182
frederikhors <41120635+frederikhors@users.noreply.github.com>
2021-01-17 20:39:23 +0100

    add postcss and tailwind: it doesn't work

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const production = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('postcss-simple-vars'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...(production ? [require('cssnano')] : [])
+    ],
+};
