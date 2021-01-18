08cf7f3ce1c8bec2b73fd88d73ac371dc54a075d
frederikhors <41120635+frederikhors@users.noreply.github.com>
2021-01-18 10:24:12 +0100

    disable postcss-simple-vars

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const production = process.env.NODE_ENV === 'production'
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('postcss-simple-vars'),
+        // require('postcss-simple-vars'),
         require('tailwindcss'),
         require('autoprefixer'),
         ...(production ? [require('cssnano')] : [])
